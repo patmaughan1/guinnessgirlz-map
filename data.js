034369a3d7657beccbb838cc64514e886d1a14c1
patmaughan1 <pat7.maughan@outlook.com>
2025-08-06 10:56:44 -0700

    Create data.js

---
+++ data.js
@@ -0,0 +1,89 @@
+// Data for GuinnessGirlz reviews (sample set)
+// Each object represents a reviewed pub/location.
+// Additional locations can be added easily by
+// appending new objects to this array with the
+// same keys: name, city, country, lat, lon, rating, link.
+
+const locations = [
+  {
+    name: "The Devonshire",
+    city: "London (Soho)",
+    country: "UK",
+    lat: 51.5112,
+    lon: -0.1344,
+    rating: 5.0,
+    link: "https://instagram.com/guinnessgirlz"
+  },
+  {
+    name: "Packhorse and Talbot",
+    city: "London (Chiswick)",
+    country: "UK",
+    lat: 51.4927,
+    lon: -0.2673,
+    rating: 4.0,
+    link: "https://instagram.com/guinnessgirlz"
+  },
+  {
+    name: "The Sun Pub",
+    city: "London (Clapham)",
+    country: "UK",
+    lat: 51.4647,
+    lon: -0.1399,
+    rating: 4.0,
+    link: "https://instagram.com/guinnessgirlz"
+  },
+  {
+    name: "The Whitford",
+    city: "Wexford",
+    country: "Ireland",
+    lat: 52.3423,
+    lon: -6.4779,
+    rating: 4.9,
+    link: "https://instagram.com/guinnessgirlz"
+  },
+  {
+    name: "Galway Irish Pub",
+    city: "Paris",
+    country: "France",
+    lat: 48.8531,
+    lon: 2.3499,
+    rating: 2.0,
+    link: "https://instagram.com/guinnessgirlz"
+  },
+  {
+    name: "The Dove",
+    city: "London (Hammersmith)",
+    country: "UK",
+    lat: 51.4847,
+    lon: -0.2233,
+    rating: 4.0,
+    link: "https://instagram.com/guinnessgirlz"
+  },
+  {
+    name: "The Hope Fitzrovia",
+    city: "London (Fitzrovia)",
+    country: "UK",
+    lat: 51.5190,
+    lon: -0.1345,
+    rating: 5.0,
+    link: "https://instagram.com/guinnessgirlz"
+  },
+  {
+    name: "Prince Albert",
+    city: "London (Battersea)",
+    country: "UK",
+    lat: 51.4803,
+    lon: -0.1622,
+    rating: 4.0,
+    link: "https://instagram.com/guinnessgirlz"
+  },
+  {
+    name: "Malones Leith",
+    city: "Edinburgh (Leith)",
+    country: "UK",
+    lat: 55.9751,
+    lon: -3.1715,
+    rating: 3.0,
+    link: "https://instagram.com/guinnessgirlz"
+  }
+];
